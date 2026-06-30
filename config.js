676b6cc8213265d462f263074b4bb70eeba5dec0
kushanthag-maker <kushanthag@gmail.com>
2026-06-30 23:25:15 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    BOT_NAME: "GPT-X",
+    OWNER: "94769904294",
+
+    MENU_IMAGE: "https://files.catbox.moe/rqpku2.jpg",
+    MENU_VIDEO: "https://files.catbox.moe/ze3uuc.mp4",
+
+    PREFIX: "."
+};
