fc88e61d89ffcefd490014e76b94db322a36e316
kushanthag-maker <kushanthag@gmail.com>
2026-06-30 23:46:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
     BOT_NAME: "GPT-X",
-    OWNER: "94769904294",
+    PREFIX: ".",
+
+    OWNER_NAME: "BLCKA",
+    OWNER_NUMBER: "94769904294",
 
     MENU_IMAGE: "https://files.catbox.moe/rqpku2.jpg",
     MENU_VIDEO: "https://files.catbox.moe/ze3uuc.mp4",
 
-    PREFIX: "."
+    SESSION_FOLDER: "./session"
 };
