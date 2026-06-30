b8e42162a90f48ddaf9e64a208eb4952d5b750dc
kushanthag-maker <kushanthag@gmail.com>
2026-06-30 23:26:01 +0530

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "alive",
+
+    async execute({ socket, jid }) {
+        await socket.sendMessage(jid, {
+            text: "🤖 GPT-X Bot is Alive and Running!"
+        });
+    }
+};
